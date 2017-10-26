4cbb6ee70be9bb9196ffb13145bfd2077977fb26
rupeshbhatti <rupesh.bhatti@hotmail.com>
2017-10-26 10:23:58 +0100

    Seeding more data for heroku

--- db/seeds.js
+++ db/seeds.js
@@ -30,55 +30,54 @@ User
           createdBy: users[0],
           content: 'This was the shiznit'
         }
-      // },{
-      //   toonName: 'BraveStarr',
-      //   imageFilepath: 'http://statici.behindthevoiceactors.com/behindthevoiceactors/_img/shows/banner_570.jpg',
-      //   createdBy: users[0]
-      // },{
-      //   toonName: 'Inspector gadget',
-      //   imageFilepath: 'http://www.liketotally80s.com/wp-content/uploads/2014/08/rbwc-inspector-gadget.jpg',
-      //   createdBy: users[0]
-      // },{
-      //   toonName: 'Alvin and The Chipmunks',
-      //   imageFilepath: 'http://www.liketotally80s.com/wp-content/uploads/2014/08/rbwc-chipmunks.jpg',
-      //   createdBy: users[0],
-      //   comments: {
-      //     createdBy: users[0],
-      //     content: 'This was the shiznit'
-      //   }
-      // },{
-      //   toonName: 'The Jetsons',
-      //   imageFilepath: 'http://www.liketotally80s.com/wp-content/uploads/2014/08/rbwc-jetsons.jpg',
-      //   createdBy: users[0]
-      // },{
-      //   toonName: 'The Smurfs',
-      //   imageFilepath: 'http://www.liketotally80s.com/wp-content/uploads/2014/08/smurfs-2.jpg',
-      //   createdBy: users[0]
-      // },{
-      //   toonName: 'Thundercats',
-      //   imageFilepath: 'https://vignette2.wikia.nocookie.net/thundercats/images/7/7e/619px-Vlcsnap-2014-03-12-21h52m32s82.jpg',
-      //   createdBy: users[0]
-      // },{
-      //   toonName: 'Teenage mutant ninja turtles',
-      //   imageFilepath: 'http://cdn.playbuzz.com/cdn/447ac729-b9c8-465e-8175-d2451e9a003d/be02891d-9188-40e7-8056-12057e371687.jpg',
-      //   createdBy:users[0]
-      // },{
-      //   toonName: 'Danger mouse',
-      //   imageFilepath: 'https://ichef.bbci.co.uk/childrens-responsive-ichef-live/r/400/1x/cbbc/DM-Ultimate-er-SP_16X9.jpg',
-      //   createdBy: users[0]
-      // },{
-      //   toonName: 'Hong Kong Phooey',
-      //   imageFilepath: 'https://billtaylorcsp.files.wordpress.com/2013/07/hong-kong-phooey.png',
-      //   createdBy: users[0]
-      // },{
-      //   toonName: 'Tranformers',
-      //   imageFilepath: 'http://cdn.hark.com/images/000/006/089/6089/original.0',
-      //   createdBy: users[0]
-      // },{
-      //   toonName: 'Bananaman',
-      //   imageFilepath: 'http://i4.mirror.co.uk/incoming/article3259984.ece/ALTERNATES/s1200/bananaman-TEASER-ONLY.jpg',
-      //   createdBy: users[0]
-      // },{
+      },{
+        toonName: 'BraveStarr',
+        imageFilepath: 'http://statici.behindthevoiceactors.com/behindthevoiceactors/_img/shows/banner_570.jpg',
+        createdBy: users[0]
+      },{
+        toonName: 'Inspector gadget',
+        imageFilepath: 'http://www.liketotally80s.com/wp-content/uploads/2014/08/rbwc-inspector-gadget.jpg',
+        createdBy: users[0]
+      },{
+        toonName: 'Alvin and The Chipmunks',
+        imageFilepath: 'http://www.liketotally80s.com/wp-content/uploads/2014/08/rbwc-chipmunks.jpg',
+        createdBy: users[0],
+        comments: {
+          createdBy: users[0],
+          content: 'This was the shiznit'
+        }
+      },{
+        toonName: 'The Jetsons',
+        imageFilepath: 'http://www.liketotally80s.com/wp-content/uploads/2014/08/rbwc-jetsons.jpg',
+        createdBy: users[0]
+      },{
+        toonName: 'The Smurfs',
+        imageFilepath: 'http://www.liketotally80s.com/wp-content/uploads/2014/08/smurfs-2.jpg',
+        createdBy: users[0]
+      },{
+        toonName: 'Thundercats',
+        imageFilepath: 'https://vignette2.wikia.nocookie.net/thundercats/images/7/7e/619px-Vlcsnap-2014-03-12-21h52m32s82.jpg',
+        createdBy: users[0]
+      },{
+        toonName: 'Teenage mutant ninja turtles',
+        imageFilepath: 'http://cdn.playbuzz.com/cdn/447ac729-b9c8-465e-8175-d2451e9a003d/be02891d-9188-40e7-8056-12057e371687.jpg',
+        createdBy: users[0]
+      },{
+        toonName: 'Danger mouse',
+        imageFilepath: 'https://ichef.bbci.co.uk/childrens-responsive-ichef-live/r/400/1x/cbbc/DM-Ultimate-er-SP_16X9.jpg',
+        createdBy: users[0]
+      },{
+        toonName: 'Hong Kong Phooey',
+        imageFilepath: 'https://billtaylorcsp.files.wordpress.com/2013/07/hong-kong-phooey.png',
+        createdBy: users[0]
+      },{
+        toonName: 'Tranformers',
+        imageFilepath: 'http://cdn.hark.com/images/000/006/089/6089/original.0',
+        createdBy: users[0]
+      },{
+        toonName: 'Bananaman',
+        imageFilepath: 'http://i4.mirror.co.uk/incoming/article3259984.ece/ALTERNATES/s1200/bananaman-TEASER-ONLY.jpg',
+        createdBy: users[0]
       },{
         toonName: 'Superted',
         imageFilepath: 'https://static.comicvine.com/uploads/original/13/132162/2849046-superted_l03.jpg',
